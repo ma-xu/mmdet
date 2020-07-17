99bfb69c9e578295c8b301805ebfe61521898df0
Xu Ma <xuma@my.unt.edu>
2020-07-17 13:09:06 -0500

    F1

--- mmdet/datasets/ade20k.py
+++ mmdet/datasets/ade20k.py
@@ -488,6 +488,135 @@ class ADE20KDataset(CustomDataset):
         toc = time.time()
         print('DONE (t={:0.2f}s).'.format(toc - tic))
 
+    def summarize(self,cocoEval):
+        '''
+        Compute and display summary metrics for evaluation results.
+        Note this functin can *only* be applied on the default parameter
+        setting
+        '''
+        def _summarize(ap=1, iouThr=None, areaRng='all', maxDets=100, F1=False):
+            p = cocoEval.params
+            iStr = '{:<18} {} @[ IoU={:<9} | area={:>6s} | maxDets={:>3d} ] = {:0.3f}'  # noqa: E501
+            titleStr = 'Average Precision' if ap == 1 else 'Average Recall'
+            typeStr = '(AP)' if ap == 1 else '(AR)'
+            if F1:
+                titleStr = 'Average F1Score'
+                typeStr = '(F1)'
+
+
+            iouStr = '{:0.2f}:{:0.2f}'.format(p.iouThrs[0], p.iouThrs[-1]) \
+                if iouThr is None else '{:0.2f}'.format(iouThr)
+
+            aind = [
+                i for i, aRng in enumerate(p.areaRngLbl) if aRng == areaRng
+            ]
+            mind = [i for i, mDet in enumerate(p.maxDets) if mDet == maxDets]
+            if F1:
+                precision = cocoEval.eval['precision']
+                recall = cocoEval.eval['recall']
+                if iouThr is not None:
+                    t = np.where(iouThr == p.iouThrs)[0]
+                    precision = precision[t]
+                    recall = recall[t]
+                precision = precision[:, :, :, aind, mind]
+                recall = recall[:, :, aind, mind]
+                if len(precision[precision > -1]) == 0:
+                    mean_precision = -1
+                else:
+                    mean_precision = np.mean(precision[precision > -1])
+                if len(recall[recall > -1]) == 0:
+                    mean_recall = -1
+                else:
+                    mean_recall = np.mean(recall[recall > -1])
+                mean_F1 = 2*mean_recall*mean_precision/(mean_recall+mean_precision+np. np.spacing(1))
+                print(
+                    iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets,
+                                mean_F1))
+                return mean_F1
+
+            if ap == 1:
+                # dimension of precision: [TxRxKxAxM]
+                s = cocoEval.eval['precision']
+                # IoU
+                if iouThr is not None:
+                    t = np.where(iouThr == p.iouThrs)[0]
+                    s = s[t]
+                s = s[:, :, :, aind, mind]
+            else:
+                # dimension of recall: [TxKxAxM]
+                s = cocoEval.eval['recall']
+                if iouThr is not None:
+                    t = np.where(iouThr == p.iouThrs)[0]
+                    s = s[t]
+                s = s[:, :, aind, mind]
+            if len(s[s > -1]) == 0:
+                mean_s = -1
+            else:
+                mean_s = np.mean(s[s > -1])
+            print(
+                iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets,
+                            mean_s))
+            return mean_s
+
+        def _summarizeDets():
+            stats = np.zeros((15, ))
+            stats[0] = _summarize(1)
+            stats[1] = _summarize(1, iouThr=.5, maxDets=cocoEval.params.maxDets[2])
+            stats[2] = _summarize(1,
+                                  iouThr=.75,
+                                  maxDets=cocoEval.params.maxDets[2])
+            stats[3] = _summarize(1,
+                                  areaRng='small',
+                                  maxDets=cocoEval.params.maxDets[2])
+            stats[4] = _summarize(1,
+                                  areaRng='medium',
+                                  maxDets=cocoEval.params.maxDets[2])
+            stats[5] = _summarize(1,
+                                  areaRng='large',
+                                  maxDets=cocoEval.params.maxDets[2])
+            stats[6] = _summarize(0, maxDets=cocoEval.params.maxDets[0])
+            stats[7] = _summarize(0, maxDets=cocoEval.params.maxDets[1])
+            stats[8] = _summarize(0, maxDets=cocoEval.params.maxDets[2])
+            stats[9] = _summarize(0,
+                                  areaRng='small',
+                                  maxDets=cocoEval.params.maxDets[2])
+            stats[10] = _summarize(0,
+                                   areaRng='medium',
+                                   maxDets=cocoEval.params.maxDets[2])
+            stats[11] = _summarize(0,
+                                   areaRng='large',
+                                   maxDets=cocoEval.params.maxDets[2])
+            stats[12] = _summarize(1,F1=True)
+            stats[13] = _summarize(1, iouThr=.5, maxDets=cocoEval.params.maxDets[2],F1=True)
+            stats[14] = _summarize(1,
+                                  iouThr=.75,
+                                  maxDets=cocoEval.params.maxDets[2],F1=True)
+            return stats
+
+        def _summarizeKps():
+            stats = np.zeros((10, ))
+            stats[0] = _summarize(1, maxDets=20)
+            stats[1] = _summarize(1, maxDets=20, iouThr=.5)
+            stats[2] = _summarize(1, maxDets=20, iouThr=.75)
+            stats[3] = _summarize(1, maxDets=20, areaRng='medium')
+            stats[4] = _summarize(1, maxDets=20, areaRng='large')
+            stats[5] = _summarize(0, maxDets=20)
+            stats[6] = _summarize(0, maxDets=20, iouThr=.5)
+            stats[7] = _summarize(0, maxDets=20, iouThr=.75)
+            stats[8] = _summarize(0, maxDets=20, areaRng='medium')
+            stats[9] = _summarize(0, maxDets=20, areaRng='large')
+            return stats
+
+        if not cocoEval.eval:
+            raise Exception('Please run accumulate() first')
+        iouType = cocoEval.params.iouType
+        if iouType == 'segm' or iouType == 'bbox':
+            summarize = _summarizeDets
+        elif iouType == 'keypoints':
+            summarize = _summarizeKps
+        cocoEval.stats = summarize()
+
+
     def evaluate(self,
                  results,
                  metric='bbox',
@@ -584,9 +713,12 @@ class ADE20KDataset(CustomDataset):
                 print("my accumulate start")
                 self.accumulate(cocoEval)
                 print("my accumulate end")
-                print("summarize start")
-                cocoEval.summarize()
-                print("summarize end")
+                # print("summarize start")
+                # cocoEval.summarize()
+                # print("summarize end")
+                print("my summarize start")
+                self.summarize(cocoEval)
+                print("my summarize end")
 
                 if classwise:  # Compute per-category AP
                     # Compute per-category AP
