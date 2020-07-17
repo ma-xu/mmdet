b1b22e6603c60635c89014af6a2cb0af85554c28
Xu Ma <xuma@my.unt.edu>
2020-07-17 10:58:49 -0500

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -70,7 +70,8 @@ def parse_args():
 
 def main():
     args = parse_args()
-    warnings.simplefilter('ignore',UserWarning)
+
+    warnings.filterwarnings('ignore')
 
     assert args.out or args.eval or args.format_only or args.show \
         or args.show_dir, \
