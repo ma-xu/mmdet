cad3792e3ddea79e3737256be2253d7e4f497a0e
Xu Ma <xuma@my.unt.edu>
2020-07-31 11:36:39 -0500

    Update openmax_helper.py

--- helper/openmax_helper.py
+++ helper/openmax_helper.py
@@ -20,8 +20,8 @@ from mmdet.helper.openmax import *
 def parse_args():
     parser = argparse.ArgumentParser(description='Openmax Helper')
     parser.add_argument('config', help='test config file path')
-    parser.add_argument('centroids', default='/home/xuma/mmdet/centroids.pkl', help='centroids path')
-    parser.add_argument('score_path', default='/home/xuma/mmdet/scores_', help='score path')
+    parser.add_argument('--centroids', default='/home/xuma/mmdet/centroids.pkl', help='centroids path')
+    parser.add_argument('--score_path', default='/home/xuma/mmdet/scores_', help='score path')
     args = parser.parse_args()
     if 'LOCAL_RANK' not in os.environ:
         os.environ['LOCAL_RANK'] = str(args.local_rank)
